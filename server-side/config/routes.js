90dc6fc5a361a258a7819ccdf19c84e9f7c23c68
superterminal <trendemil@gmail.com>
2019-12-09 21:35:47 +0200

    added withAuth route

--- server-side/config/routes.js
+++ server-side/config/routes.js
@@ -1,10 +1,13 @@
 const router = require('../routes/');
+const withAuth = require('../utils/withAuth');
 
 module.exports = (app) => {
 
     app.use('/api/user', router.user);
 
-    app.use('/api/origami', router.origami);
+    app.get('/auth', withAuth, function(req, res) {
+        res.sendStatus(200);
+    })
 
     app.use('*', (req, res, next) => res.send('<h1> Something went wrong. Try again. :thumbsup: </h1>'))
 };
