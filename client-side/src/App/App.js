ea7ac2b59013bfa1b44dbb930c593d2fbf6f4bb3
superterminal <trendemil@gmail.com>
2019-12-09 13:18:42 +0200

    added base app login and home

---
+++ client-side/src/App/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import Navbar from '../Navbar/Navbar';
+import Footer from '../Footer/Footer';
+import Login from '../Login/Login';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+
+
+      <Footer />
+    </div>
+  );
+}
+
+
+export default App;
