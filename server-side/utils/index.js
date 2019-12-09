646ade9359a2bc13185b1183ed960fbbcbc3d1f6
superterminal <trendemil@gmail.com>
2019-12-09 21:36:22 +0200

    added withAuth method for unauthorized access

--- server-side/utils/index.js
+++ server-side/utils/index.js
@@ -1,7 +1,9 @@
 const jwt = require('./jwt');
 const auth = require('./auth');
+const withAuth = require('./withAuth');
 
 module.exports = {
     jwt,
-    auth
+    auth,
+    withAuth
 };
---
+++ server-side/utils/withAuth.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+const secret = 'shhhhh';
+const config = require('../config/config');
+
+const withAuth = function (req, res, next) {
+ 
+    const token = req.cookies[config.authCookieName] || '';
+
+    if (!token) {
+        res.status(401).send('Unauthorized: No token provided');
+    } else {
+        jwt.verify(token, secret, function (err, decoded) {
+            if (err) {
+                res.status(401).send('Unauthorized: Invalid token');
+            } else {
+                req.username = decoded.username;
+                next();
+            }
+        });
+    }
+}
+
+module.exports = withAuth;
