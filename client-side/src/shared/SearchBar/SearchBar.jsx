063082a490473770a24d91efaaad3bb8f51af5fd
superterminal <trendemil@gmail.com>
2019-12-09 18:21:59 +0200

    added search bar component and Link

---
+++ client-side/src/shared/SearchBar/SearchBar.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import './SearchBar.css';
+
+import recipeSearchService from '../../services/recipe-search-service';
+
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: '',
+      result: {},
+      loading: false
+    }
+  }
+
+  changeHandler = (e) => {
+    const query = e.target.value;
+    this.setState({
+      query,
+      result: {},
+      loading: false
+    });
+    
+  }
+
+  submitSearch = (e) => {
+    e.preventDefault();
+    let query = this.state.query;
+    if (!query) {
+      this.setState({
+        query,
+        result: {},
+        loading: false
+      });
+    } else {
+      this.setState({query, message: '', loading: true}, () => {
+        recipeSearchService.findByQuery(query).then(res => {
+          console.log(res);
+        })
+      })
+    }
+  }
+
+  render() {
+    return (
+      <div className="wrap">
+        <form className="search" onSubmit={this.submitSearch}>
+            <input type="text" className="searchTerm" placeholder="What are you looking for?" onChange={this.changeHandler}/>
+            <button type="submit" className="searchButton">
+              Search
+          </button>
+        </form>
+      </div>
+  );
+  }  
+}
+
+export default SearchBar;
