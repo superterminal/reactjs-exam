2ac2b24ef42bf796001d7a65994121a66d4d7f32
superterminal <trendemil@gmail.com>
2019-12-10 19:20:23 +0200

    added loader

---
+++ client-side/src/shared/Loader/Loader.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Loader.css';
+
+function Loader() {
+    return <div className="loadingio-spinner-pulse-c3nzgmc2ozs">
+        <div className="ldio-mt000bhgsrj">
+            <div></div><div></div><div></div>
+        </div></div>
+}
+
+export default Loader;
