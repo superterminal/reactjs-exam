f47dc820211fd8292e2d65abbd5baca58153dca0
superterminal <trendemil@gmail.com>
2019-12-10 18:20:37 +0200

    added basic detail info (must finish)

--- client-side/src/shared/SearchBar/Recipe/Details/Details.jsx
+++ client-side/src/shared/SearchBar/Recipe/Details/Details.jsx
@@ -1,112 +1,101 @@
 import React from 'react'
 import './Details.css';
 
+import recipeSearchService from '../../../../services/recipe-search-service';
+
 class Details extends React.Component {
 
     constructor(props) {
         super(props);
         this.recipe = this.props.location.state.recipe;
+        this.state = {
+            recipe: {},
+            loading: false
+        };
+    }
+
+    componentDidMount() {
+        recipeSearchService.findRecipeById(this.recipe.id)
+            .then(res => this.setState({
+                recipe: res,
+                loading: true
+            }));
     }
 
     render() {
+        const { recipe } = this.state;
         return (
-            <div class='container product-details-page'>
-                <div class='row'>
-                    <div class="col-md-8">
-                        <div class="item"><img src={this.recipe.image} /></div>
+            <div className='container product-details-page'>
+                <div className='row'>
+                    <div className="col-md-6">
+                        <div className="item"><img src={recipe.image} /></div>
                     </div>
-                    <div class="col-md-4">
-                        <h2>{this.recipe.label}</h2>
-                        <h5>Product sub title</h5>
-                        <div class="price">
-                            <p>$ 1900.00 <del>$ 2000.00</del></p>
-                        </div>
+                    <div className="col-md-6">
+                        <h2>{recipe.title}</h2>
                         <hr/>
-                        <div class="product-info">
-                            <div class='row'>
-                            <div class="col-md-6">
-                                <p class="title">manufacturer:</p>
+                        <div className="product-info">
+                            <div className='row'>
+                            <div className="col-md-6">
+                                <p className="title">Cheap:</p>
                             </div>
-                            <div class="col-md-6">
-                                <p class="data"> Brand Name</p>
-                            </div>
-                            </div>
-                            <div class='row'>
-                            <div class="col-md-6">
-                                <p class="title">Materials:</p>
-                            </div>
-                            <div class="col-md-6">
-                                <p class="data"> Brand Name</p>
+                            <div className="col-md-6">
+                                <p className="data"> {recipe.cheap ? 'Yes' : 'No'}</p>
                             </div>
                             </div>
-                            <div class='row'>
-                            <div class="col-md-6">
-                                <p class="title">Availability:</p>
+                            <div className='row'>
+                                <div className="col-md-6">
+                                    <p className="title">Dairy free:</p>
+                                </div>
+                                <div className="col-md-6">
+                                    <p className="data"> {recipe.dairyFree ? 'Yes' : 'No'}</p>
+                                </div>
                             </div>
-                            <div class="col-md-6">
-                                <p class="data"><i class="far fa-check-circle"></i> In stock</p>
-                            </div>
+                            <div className='row'>
+                                <div className="col-md-6">
+                                    <p className="title">Vegetarian:</p>
+                                </div>
+                                <div className="col-md-6">
+                                    <p className="data">{recipe.vegetarian ? 'Yes' : 'No'}</p>
+                                </div>
                             </div>
                         </div>
                         <hr/>
-                        <div class="product-info">
-                            <div class='row'>
-                            <div class="col-md-6">
-                                <p class="title">Favourites:</p>
+                        <div className="product-info">
+                            <h1>TODO: (add to my to cook list in profile)</h1>
+                            <div className='row'>
+                            <div className="col-md-6">
+                                <p className="title">Ingredients:</p>
                             </div>
-                            <div class="col-md-6">
-                                <p class="data"><i class="far fa-heart"></i> Add to favourites</p>
-                            </div>
-                            </div>
-                            <div class='row'>
-                            <div class="col-md-6">
-                                <p class="title">Wishlist:</p>
-                            </div>
-                            <div class="col-md-6">
-                                <p class="data"><i class="far fa-eye"></i> Add to Watch list</p>
+                            <div className="col-md-6">
+                                <p className="data"><i className="far fa-heart"></i> Add to favourites</p>
                             </div>
                             </div>
-                            <div class='row'>
-                            <div class="col-md-6">
-                                <p class="title">collection:</p>
+                            <div className='row'>
+                            <div className="col-md-6">
+                                <p className="title">Wishlist:</p>
                             </div>
-                            <div class="col-md-6">
-                                <p class="data"><i class="far fa-star"></i> Add to collection</p>
+                            <div className="col-md-6">
+                                <p className="data"><i className="far fa-eye"></i> Add to Watch list</p>
+                            </div>
+                            </div>
+                            <div className='row'>
+                            <div className="col-md-6">
+                                <p className="title">collection:</p>
+                            </div>
+                            <div className="col-md-6">
+                                <p className="data"><i className="far fa-star"></i> Add to collection</p>
                             </div>
                             </div>
                         </div>
                         <hr/>
-                        <div class="product-info">
-                            <div class='row'>
-                            <div class="col-md-6">
-                                <p class="title">Available Colors:</p>
-                            </div>
-                            <div class="col-md-6">
-                                <ul class="colors">
-                                <li class="red"> </li>
-                                <li class="blue active"> </li>
-                                <li class="green"> </li>
-                                <li class="yellow"> </li>
-                                </ul>
-                            </div>
-                            </div>
-                            <div class='row'>
-                            <div class="col-md-6">
-                                <p class="title">Wishlist:</p>
-                            </div>
-                            <div class="col-md-6">
-                                <ul class="sizes">
-                                <li><a href="#">S</a></li>
-                                <li class="active"><a href="#">M</a></li>
-                                <li><a href="#">L</a></li>
-                                <li><a href="#">XL</a></li>
-                                </ul>
-                            </div>
-                            </div>
-                        </div>
                         </div>
                 </div>
-
+                <div className="row">
+                    <h2>How to cook:</h2>
+                    <p>
+                        {recipe.instructions}
+                    </p>
+                </div>
             </div>
         )
     }
