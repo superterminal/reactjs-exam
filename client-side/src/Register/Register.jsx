a5db3ecce4df79bc4924791d20b508f05031c62d
superterminal <trendemil@gmail.com>
2019-12-11 00:02:44 +0200

    added register fields validation (kinda)

--- client-side/src/Register/Register.jsx
+++ client-side/src/Register/Register.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './Register.css';
 
+import * as yup from 'yup';
+
 class Register extends React.Component {
     
     constructor(props) {
@@ -8,37 +10,98 @@ class Register extends React.Component {
         this.state = {
             username: '',
             password: '',
-            rePassword: ''
+            rePassword: '',
+            usernameErr: '',
+            passwordErr: '',
+            rePasswordErr: '',
+            isValid: false
         };
     }
     
-    handleChange = e => {
-        const { value, name } = e.target;
-        this.setState({
-            [name]: value
+    handleUsername = event => {
+        this.setState({ username: event.target.value }, () => {
+            this.validateUsername();
         });
+    };
+
+    handlePassword = event => {
+        this.setState({ password: event.target.value }, () => {
+            this.validatePassword();
+        });
+    };
+
+    handleRepeatPassword = event => {
+        this.setState({ rePassword: event.target.value }, () => {
+            this.validateRePassword();
+        });
+    }
+    
+    validateUsername = () => {
+        const { username } = this.state;
+        return schema.fields['username'].validate(username, { abortEarly: false })
+            .then(res => {
+                this.setState({ usernameErr: ''})
+            })
+            .catch(err => {
+                this.setState({ usernameErr: err.errors[0]})
+            });
+    }
+    
+    validatePassword = () => {
+        const { password } = this.state;
+        return schema.fields['password'].validate(password, { abortEarly: false })
+            .then(res => {
+                this.setState({ passwordErr: ''})
+            })
+            .catch(err => {
+                this.setState({ passwordErr: err.errors[0]})
+            });
+    }
+
+    validateRePassword = () => {
+        const { password, rePassword } = this.state;
+
+        if (password !== rePassword) {
+            this.setState({ rePasswordErr: 'Password mismatch'})
+            return;
+        } else {
+            this.setState({ rePasswordErr: ''})
+        }
+
+        return schema.fields['rePassword'].validate(rePassword, { abortEarly: false })
+            .then(res => {
+                this.setState({ rePasswordErr: ''})
+            })
+            .catch(err => {
+                this.setState({ rePasswordErr: err.errors[0]})
+            });
     }
 
     onSubmit = e => {
         e.preventDefault();
-        fetch('http://localhost:9999/api/user/register', {
-            method: 'POST',
-            body: JSON.stringify(this.state),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        }).then(res => {
-            if (res.status === 200) {
-                this.props.history.push('/login');
-            } else {
-                const error = new Error(res.error);
-                console.log(error);
-                throw error;
-            }
-        }).catch(err => {
-            console.log(err);
-            alert('Error with registration please try again later');
-        })
+        const { usernameErr, passwordErr, rePasswordErr } = this.state;
+        const isValid = !usernameErr && !passwordErr && !rePasswordErr;
+        
+        if (isValid) {
+            fetch('http://localhost:9999/api/user/register', {
+                method: 'POST',
+                body: JSON.stringify(this.state),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }).then(res => {
+                if (res.status === 200) {
+                    this.props.history.push('/login');
+                } else {
+                    const error = new Error(res.error);
+                    // handle user already exists
+                    console.log(error);
+                }
+            }).catch(err => {
+                console.log(err);
+                alert('Error with registration please try again later');
+            })
+        }
     }
 
 
@@ -47,39 +110,60 @@ class Register extends React.Component {
             <div className="text-center">
                 <form className="form-signin" onSubmit={this.onSubmit}>
                     <h1 className="h3 mb-3 font-weight-normal">Register here!</h1>
+                    {this.state.usernameErr}
                     <label htmlFor="inputEmail" className="sr-only">Username</label>
                     <input 
                         type="text"
                         name="username" 
                         className="form-control" 
                         placeholder="Username" 
-                        onChange={this.handleChange}
-                        required 
+                        onChange={this.handleUsername}
+                        onBlur={this.validateUsername}
+                        required
                         autoFocus />
                     <label htmlFor="inputPassword" className="sr-only">Password</label>
+                    {this.state.passwordErr}
                     <input 
                         type="password" 
                         name="password"
                         className="form-control" 
                         placeholder="Password" 
-                        onChange={this.handleChange}
-                        required />
+                        onChange={this.handlePassword}
+                        onBlur={this.validatePassword}
+                        required
+                         />
                     <label htmlFor="inputRepeatPassword" className="sr-only">Repeat password</label>
+                    {this.state.rePasswordErr}
                     <input 
                         type="password" 
                         name="rePassword"
                         className="form-control" 
                         placeholder="Repeat Password" 
-                        onChange={this.handleChange}
-                        required />
-                    <button className="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+                        onChange={this.handleRepeatPassword}
+                        onBlur={this.validateRePassword}
+                        required
+                         />
+                    <button className={!this.state.usernameErr && !this.state.passwordErr && !this.state.rePasswordErr ? "btn btn-lg btn-primary btn-block" : "btn btn-lg btn-secondary btn-block"} type="submit">Register</button>
                     <p className="mt-5 mb-3 text-muted">&copy; 2017-2019</p>
                 </form>
             </div>
         );
     }
-    
-    
 }
+
+const schema = yup.object({
+    username: yup
+        .string('Username must be a string!')
+        .required('Username is required')
+        .min(6, 'Username should be atleast 6 chars'),
+    password: yup
+        .string('Password must be a string!')
+        .required('Password is required')
+        .min(6, 'Password should be atleast 6 chars'),
+    rePassword: yup.string('Repeat Password must be a string')
+        .required('Repeat password is required')
+        .min(6, 'Repeat Password should be atleast 6 chars'),
+});
+
 
 export default Register;
