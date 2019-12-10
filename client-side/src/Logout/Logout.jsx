a7bca095d6154dc0e452eb21c3d22b0076813723
superterminal <trendemil@gmail.com>
2019-12-10 19:56:20 +0200

    added logout component

---
+++ client-side/src/Logout/Logout.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import userService from '../services/user-service';
+
+class Logout extends React.Component {
+    logout = () => {
+        userService.logout().then(() => {
+            this.props.setLoggedIn(false);
+            this.props.history.push('/');
+        });
+    }
+
+    render() {
+        this.logout();
+        return null;
+    }
+}
+
+export default Logout;
