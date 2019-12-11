2eb1efc3e9e63d2fb45595ca74868a60b0715693
superterminal <trendemil@gmail.com>
2019-12-11 14:19:11 +0200

    added product service

---
+++ client-side/src/services/product-seach-service.js
@@ -0,0 +1,14 @@
+const apiKey = process.env.REACT_APP_RECIPE_API_KEY;
+
+const productService = {
+      findProductById: function (id) {
+        return fetch(`https://api.spoonacular.com/food/ingredients/${id}/information?apiKey=${apiKey}`, {
+          method: 'GET',
+          headers: {
+            'Content-type': 'application/json'
+          }
+        }).then(res => res.json());
+      }
+  };
+  
+  export default productService;
