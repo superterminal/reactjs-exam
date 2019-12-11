a823406fdf2cafaca6cde14ae59f51c93a4b58ff
superterminal <trendemil@gmail.com>
2019-12-12 00:18:35 +0200

    added advanced search and my recipies in navbar

--- client-side/src/Navbar/Navbar.jsx
+++ client-side/src/Navbar/Navbar.jsx
@@ -9,7 +9,8 @@ function Navbar({ loggedIn }) {
         <button type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" className="navbar-toggler navbar-toggler-right"><span></span><span></span><span></span></button>
         <div id="navbarSupportedContent" className="collapse navbar-collapse">
           <ul className="navbar-nav ml-auto align-items-start align-items-lg-center">
-            {loggedIn && <Link to="/profile" className="nav-link link-scroll">Profile</Link>}
+            {loggedIn && <Link to="/my-recipies" className="nav-link link-scroll">My Recipies</Link>}
+            {loggedIn && <Link to="/advanced-search" className="nav-link link-scroll">Advanced search</Link>}
             {loggedIn && <Link to="/logout" className="nav-link link-scroll">Logout</Link>}
             {!loggedIn && <Link to="/login" className="nav-link link-scroll">Login</Link>}
             {!loggedIn && <Link to="/register" className="nav-link link-scroll">Register</Link>}
