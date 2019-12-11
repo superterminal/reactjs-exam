20130643317dd7e219468b747ed573fb7762caf4
superterminal <trendemil@gmail.com>
2019-12-12 00:19:12 +0200

    added findByIngredients and findByNutrients methods in service

--- client-side/src/services/recipe-search-service.js
+++ client-side/src/services/recipe-search-service.js
@@ -9,6 +9,25 @@ const recipeSearchService = {
           }
         }).then(res => res.json());
       },
+      findByNutrients: function (nutrient, min, max) {
+        console.log(nutrient, min, max) 
+        let minNutrient = 'min' + nutrient;
+        let maxNutrient = 'max' + nutrient;
+        return fetch(`https://api.spoonacular.com/recipes/findByNutrients?${minNutrient}=${+min}&${maxNutrient}=${+max}&apiKey=${apiKey} `, {
+          method: 'GET',
+          headers: {
+            'Content-type': 'application/json'
+          }
+        }).then(res => res.json());
+      },
+      findByIngredients: function (query) {
+        return fetch(`https://api.spoonacular.com/recipes/findByIngredients?ingredients=${query}&apiKey=${apiKey} `, {
+          method: 'GET',
+          headers: {
+            'Content-type': 'application/json'
+          }
+        }).then(res => res.json());
+      },
       findRecipeById: function (id) {
         return fetch(`https://api.spoonacular.com/recipes/${id}/information?includeNutrition=false&apiKey=${apiKey} `, {
           method: 'GET',
@@ -16,7 +35,7 @@ const recipeSearchService = {
             'Content-type': 'application/json'
           }
         }).then(res => res.json());
-      }
+      },
   };
   
   export default recipeSearchService;
