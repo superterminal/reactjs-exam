08cf3c86a685d82cb7542efc7c3c1ef96e81a93f
superterminal <trendemil@gmail.com>
2019-12-12 11:38:29 +0200

    added random recipies in navbar

--- client-side/src/Navbar/Navbar.jsx
+++ client-side/src/Navbar/Navbar.jsx
@@ -9,6 +9,7 @@ function Navbar({ loggedIn }) {
         <button type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" className="navbar-toggler navbar-toggler-right"><span></span><span></span><span></span></button>
         <div id="navbarSupportedContent" className="collapse navbar-collapse">
           <ul className="navbar-nav ml-auto align-items-start align-items-lg-center">
+            {loggedIn && <Link to="/random-recipies" className="nav-link link-scroll">Get random recipies</Link>}
             {loggedIn && <Link to="/my-recipies" className="nav-link link-scroll">My Recipies</Link>}
             {loggedIn && <Link to="/advanced-search" className="nav-link link-scroll">Advanced search</Link>}
             {loggedIn && <Link to="/logout" className="nav-link link-scroll">Logout</Link>}
