bf17ca7adb57ef39cde93e1c41e8d86852182d19
superterminal <trendemil@gmail.com>
2019-12-12 12:05:44 +0200

    added find random recipies in services

--- client-side/src/services/recipe-search-service.js
+++ client-side/src/services/recipe-search-service.js
@@ -2,7 +2,7 @@ const apiKey = process.env.REACT_APP_RECIPE_API_KEY;
 
 const recipeSearchService = {
     findByQuery: function (query) {
-        return fetch(`https://api.spoonacular.com/recipes/search?query=${query}&number=10&apiKey=${apiKey} `, {
+        return fetch(`https://api.spoonacular.com/recipes/search?query=${query}&number=10&apiKey=${apiKey}`, {
           method: 'GET',
           headers: {
             'Content-type': 'application/json'
@@ -13,7 +13,7 @@ const recipeSearchService = {
         console.log(nutrient, min, max) 
         let minNutrient = 'min' + nutrient;
         let maxNutrient = 'max' + nutrient;
-        return fetch(`https://api.spoonacular.com/recipes/findByNutrients?${minNutrient}=${+min}&${maxNutrient}=${+max}&apiKey=${apiKey} `, {
+        return fetch(`https://api.spoonacular.com/recipes/findByNutrients?${minNutrient}=${+min}&${maxNutrient}=${+max}&apiKey=${apiKey}`, {
           method: 'GET',
           headers: {
             'Content-type': 'application/json'
@@ -21,7 +21,7 @@ const recipeSearchService = {
         }).then(res => res.json());
       },
       findByIngredients: function (query) {
-        return fetch(`https://api.spoonacular.com/recipes/findByIngredients?ingredients=${query}&apiKey=${apiKey} `, {
+        return fetch(`https://api.spoonacular.com/recipes/findByIngredients?ingredients=${query}&apiKey=${apiKey}`, {
           method: 'GET',
           headers: {
             'Content-type': 'application/json'
@@ -29,13 +29,21 @@ const recipeSearchService = {
         }).then(res => res.json());
       },
       findRecipeById: function (id) {
-        return fetch(`https://api.spoonacular.com/recipes/${id}/information?includeNutrition=false&apiKey=${apiKey} `, {
+        return fetch(`https://api.spoonacular.com/recipes/${id}/information?includeNutrition=false&apiKey=${apiKey}`, {
           method: 'GET',
           headers: {
             'Content-type': 'application/json'
           }
         }).then(res => res.json());
       },
+      getRandomRecipies: function () {
+        return fetch(`https://api.spoonacular.com/recipes/random?number=9&apiKey=${apiKey}`, {
+          method: 'GET',
+          headers: {
+            'Content-type': 'application/json'
+          }
+        }).then(res => res.json());
+      }
   };
   
   export default recipeSearchService;
