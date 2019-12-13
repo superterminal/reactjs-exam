a587fd0efd3e3e64bec2115f3ed4a33de1e06db0
superterminal <trendemil@gmail.com>
2019-12-13 17:23:50 +0200

    added jokes

--- client-side/src/AdvancedSearch/AdvancedSearch.jsx
+++ client-side/src/AdvancedSearch/AdvancedSearch.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './AdvancedSearch.css';
 
 import AdvancedSearchBar from './AdvancedSearchBar/AdvancedSearchBar';
+import FoodJokeOrTrivia from '../shared/FoodJokeOrTrivia/FoodJokeOrTrivia';
 
 function AdvancedSearch() {
     return <div className="advancedSearch">
             <h1>Advanced browsing is here</h1>
             <AdvancedSearchBar />
+            <FoodJokeOrTrivia opt={'jokes'} />
         </div>
 }
 
