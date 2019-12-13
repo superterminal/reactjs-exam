4436cb2e13a6369c6ca5009f2a3eb50df02eee10
superterminal <trendemil@gmail.com>
2019-12-13 17:24:42 +0200

    added jokes

--- client-side/src/MealPlan/MealPlan.jsx
+++ client-side/src/MealPlan/MealPlan.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './MealPlan.css';
 
 import MealPlanSearchBar from './MealPlanSearchBar/MealPlanSearchBar';
+import FoodJokeOrTrivia from '../shared/FoodJokeOrTrivia/FoodJokeOrTrivia';
 
 function MealPlan() {
     return <div className="mealPlan">
             <h1>Generate your own meal plan here!</h1>
             <h3>Please check a few params first:</h3>  
             <MealPlanSearchBar />
+            {<FoodJokeOrTrivia opt={'jokes'} />}
         </div>
 }
 
