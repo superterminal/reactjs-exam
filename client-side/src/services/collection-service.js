222455e9e520a727920008ba9723876210a35210
superterminal <trendemil@gmail.com>
2019-12-13 22:47:50 +0200

    added service for user collection

---
+++ client-side/src/services/collection-service.js
@@ -0,0 +1,43 @@
+const collectionService = {
+    getRecipe: function (id) {
+        return fetch(`http://localhost:9999/api/recipe/${id}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }, 
+            credentials: 'include'
+        }).then(res => res.json())
+        .catch(err => console.error(err));
+    },
+    getAllRecipies: function () {
+        return fetch(`http://localhost:9999/api/recipe`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            credentials: 'include'
+        }).then(res => res.json())
+        .catch(err => console.error(err));
+    },
+    addToCollection: function (recipe) {
+        console.log(recipe)
+        return fetch('http://localhost:9999/api/recipe', {
+            method: 'POST',
+            body: JSON.stringify(recipe),
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            credentials: 'include'
+        }).then(res => res.json())
+        .catch(err => console.error(err));
+    },
+    deleteFromCollection: function (recipeId) {
+        return fetch(`http://localhost:9999/api/recipe/${recipeId}`, {
+            method: 'DELETE',
+            credentials: 'include'
+        }).then(res => res.json())
+        .catch(err => console.error(err));
+    }
+}
+
+export default collectionService;
