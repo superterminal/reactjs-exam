ce8eca42de4cd27a86c3adb97ebd94e577f13811
superterminal <trendemil@gmail.com>
2019-12-13 17:22:19 +0200

    added videos component

---
+++ client-side/src/Videos/Videos.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Videos.css';
+
+import VideosSearchBar from './VideosSearchBar/VideosSearchBar';
+
+function Videos() {
+    return (
+        <div>
+            <VideosSearchBar />
+        </div>
+    )
+}
+
+export default Videos;
---
+++ client-side/src/Videos/VideosSearchBar/VideosSearchBar.jsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import YouTube from 'react-youtube';
+import './VideosSearchBar.css';
+
+import Loader from '../../shared/Loader/Loader';
+import videoService from '../../services/video-service';
+import FoodJokeOrTrivia from '../../shared/FoodJokeOrTrivia/FoodJokeOrTrivia';
+
+class VideosSearchBar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            result: {},
+            query: '',
+            loading: false
+        }
+    }
+
+    queryChangeHandler = e => {
+        const query = e.target.value;
+        this.setState({
+            query,
+            result: {},
+            loading: false
+        });
+    }
+
+    submitVideoSearch = e => {
+        e.preventDefault();
+        this.setState({ query: this.state.query, loading: true }, () => {
+            setTimeout(() => {
+                videoService.getVideos(this.state.query)
+                    .then(res => {
+                        res.videos.length === 0 ? 
+                        this.setState({ result: {}, message: 'No videos found', loading: false }) 
+                        : this.setState({ result: res, loading: false });
+                    });
+            }, 1000);
+        });
+    }
+
+    renderSearchResult = () => {
+        let { result } = this.state;
+        if (Object.keys(result).length !== 0) {
+            return (result.videos.map(video => 
+                <div className="video">
+                    <h4>{video.title}</h4>
+                    <YouTube videoId={video.youTubeId} opts={{height: '250',width: '500'}}/>
+                </div>));
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h4>Search food videos!</h4>
+                <h5>Did you know that: {<FoodJokeOrTrivia opt={"trivia"} />}</h5>
+                <form className="search" onSubmit={this.submitVideoSearch}>
+                    <input type="text" className="searchTerm" placeholder="What are you looking for?, eg. pumpkin, pasta" onChange={this.queryChangeHandler}/>
+                    <button type="submit" className="searchButton">
+                        Find videos
+                    </button>
+                </form>
+                {this.state.message ? <h2>{this.state.message}</h2> : null}
+                {this.state.loading ? <Loader /> : 
+                    <div className="videos"> 
+                        {this.renderSearchResult()}
+                    </div>}
+                {/* <YouTube videoId={'9k3UaPZEIg8'} opts={{height: '250',width: '500'}} /> */}
+            </div>
+        )
+    }
+}
+
+export default VideosSearchBar;
